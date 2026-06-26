40df6ebf6f7d87e5c5ac6513a5d7e76bfbde420c
vaddi-abhi <vaddiabhi0@gmail.com>
2026-06-27 01:50:45 +0530

    fix cors for vercel deployment

--- backend/main.py
+++ backend/main.py
@@ -16,11 +16,16 @@ from routes.codeforces import (
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "https://developer-productivity-platform.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
